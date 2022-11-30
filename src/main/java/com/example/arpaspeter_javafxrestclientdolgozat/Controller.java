4fd45f233e5c62451a8017be9781278f0789589d
Árpás Péter <arpaspeter0@gmail.com>
2022-11-30 22:19:48 +0100

    egyszerűen nem értem mi a baj

--- src/main/java/com/example/arpaspeter_javafxrestclientdolgozat/Controller.java
+++ src/main/java/com/example/arpaspeter_javafxrestclientdolgozat/Controller.java
@@ -3,7 +3,7 @@ package com.example.arpaspeter_javafxrestclientdolgozat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
